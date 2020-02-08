28a990dfed4f6d1a1e4cd042e250a92986603caf
Ali <mtoshpulatov3@gmail.com>
2020-02-08 10:42:39 +0500

    completed without tests

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnInit} from '@angular/core';
+import {TodoListService} from './todo-list.service';
+import {ItemModel} from './item.model';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  activeTab = 'all';
+  public globalItems: ItemModel [] = [];
+  public items = this.globalItems;
+
+  constructor(private todoListService: TodoListService) {
+  }
+
+  ngOnInit() {
+    this.getAll();
+  }
+
+  getAll() {
+    this.globalItems = this.todoListService.getAll();
+    this.items = this.globalItems;
+  }
+
+  filter(value: string) {
+    this.activeTab = value;
+    switch (value) {
+      case 'active':
+        this.items = this.globalItems.filter(item => item.status == this.todoListService.active);
+        break;
+      case 'completed':
+        this.items = this.globalItems.filter(item => item.status == this.todoListService.completed);
+        break;
+      default:
+        this.items = this.globalItems;
+        break;
+    }
+
+  }
+
+  update(item) {
+    let index = this.items.indexOf(item);
+    if ((this.activeTab === 'active' && !item.status) || (this.activeTab === 'completed' && item.status)) {
+      this.items.splice(index, 1);
+    }
+    this.todoListService.update(item);
+  }
+
+  add(name) {
+    let item = new ItemModel(
+      Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+      name,
+      this.todoListService.active
+    );
+
+    if (this.activeTab === 'active') {
+      this.items.push(item);
+    }
+
+    this.todoListService.add(item);
+
+  }
+}
+

7cac1b59c40303b2f0fddd833f00551169f541a8
Ali <mtoshpulatov3@gmail.com>
2020-02-08 10:41:11 +0500

    completed without tests

---
+++ src/app/todo-list/todo-list.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {ItemModel} from './item.model';
+
+@Injectable()
+export class TodoListService {
+  public readonly active = 0;
+  public readonly completed = 1;
+
+  private items: ItemModel [] = [];
+
+  constructor() {
+  }
+
+  public getAll() {
+    return this.items;
+  }
+
+  add(item: ItemModel) {
+    this.items.push(item);
+  }
+
+  update(item: ItemModel) {
+    let itemIndex = this.items.indexOf(item);
+    if (itemIndex > -1) {
+      this.items[itemIndex].name = item.name;
+      this.items[itemIndex].status = +item.status;
+    }
+  }
+}
